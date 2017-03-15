5f98164b9308efd9ab80b6e9cb7fd5c529409257
Yajing Wang <wang.yajing@rytong.com>
2017-03-15 11:12:56 +0800

    新建Tomato工程

---
+++ Tomato/Tomato/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Tomato
+//
+//  Created by 王亚静 on 2017/3/15.
+//  Copyright © 2017年 Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Tomato/Tomato/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tomato
+//
+//  Created by 王亚静 on 2017/3/15.
+//  Copyright © 2017年 Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
